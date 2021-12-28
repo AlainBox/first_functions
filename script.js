fb5e3c14de09ff4a04bb62efe9db9b856b6f9170
AlainBox <93340821+AlainBox@users.noreply.github.com>
2021-12-28 22:12:33 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("Hello Winc Academy")
+
+const makingPizza = function (){
+    console.log("Make the dough");
+    console.log("Make tomato sauce");
+    console.log("Cut vegetables and meat");
+    console.log("put everything on pizza")
+    console.log("put the pizza in the oven")
+};
+
+// I am getting the result I expected (it doesn't work), because although I have defined the function in the proper way, I haven't called it.
+
+makingPizza(); // Check, it works.
+
+makingPizza();
+makingPizza();
+makingPizza();
+
+const makingSushi = function(){
+    console.log("Cook rice");
+    console.log("Fry shrimps and cut fish");
+    console.log("Cut vegetables");
+    console.log("Roll the sushi");
+    console.log("cut sushi rolls in pieces");
+};
+
+makingSushi(); // Check, it works.
+
+// Restaurant assignment
+makingSushi();
+makingSushi();
+makingSushi();
+makingPizza();
+makingPizza();
